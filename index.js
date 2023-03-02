b1ba3112b9b5edc2698bfb5a6d8a114956f1a093
Ahmar-Latif <88490785+AhmarLatif@users.noreply.github.com>
2023-03-02 15:41:31 +0500

    merging add mul

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 function add(a,b) {
     return a+b;    
     }
     console.log(add(2,3))
-=======
 function mul(a,b) {
     return a*b;    
     }
     console.log(mul(2,3))
->>>>>>> f77cfc2d479469dd8505a1c77cab96bc300b0de9
