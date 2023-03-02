8159cc2d3eba34db974b40b28116f1aad097a2f4
Ahmar-Latif <88490785+AhmarLatif@users.noreply.github.com>
2023-03-02 15:52:40 +0500

    save file

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function mul(a,b) {
     return a*b;    
     }
@@ -7,9 +6,7 @@ function div(a,b) {
     return a/b;    
     }
     console.log(div(2,3))
-=======
 function add(a,b) {
     return a+b;    
     }
     console.log(add(2,3))
->>>>>>> fe79a29b58e2bc6a3b5b4d37cfdfcce23c35c496
