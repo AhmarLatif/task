7358c73dc619b1847275c740c9cb0c11aefec769
Ahmar-Latif <88490785+AhmarLatif@users.noreply.github.com>
2023-03-02 15:48:48 +0500

    add div

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-function add(a,b) {
-    return a+b;    
+function div(a,b) {
+    return a/b;    
     }
-    console.log(add(2,3))
+    console.log(div(2,3))
+
+    function add(a,b) {
+        return a+b;    
+        }
+        console.log(add(2,3))
